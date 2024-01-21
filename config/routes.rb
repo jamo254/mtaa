58ab2a8493c69c78e1e7e29e88ed567c1fffbd47
Fred Denis <fredjamo68@gmail.com>
2024-01-21 11:56:34 +0300

    Creating app layout and setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :accounts
   # Defines the root path route ("/")
   root to: "home#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
