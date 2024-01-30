07ced7375106448d3415622fedd0739880f7d673
Fred Denis <fredjamo68@gmail.com>
2024-01-30 12:50:00 +0300

    links to user's profile

--- app/models/account.rb
+++ app/models/account.rb
@@ -3,11 +3,14 @@ class Account < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
   has_many :communities
   has_many :posts
 
-  def full_name 
+  validates_presence_of :first_name, :last_name, :username
+
+
+  def full_name
      "#{first_name} #{last_name}"
-  end 
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :accounts
-
+  get "u/:username" => "home#profile", as: :profile
   resources :communities do
     resources :posts
   end
